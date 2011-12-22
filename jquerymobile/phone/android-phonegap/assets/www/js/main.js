48d2b4b05be8a4fb434211fc7fe134b9c1fa2200
Johann Jacobsohn <johann.jacobsohn@directbox.com>
2011-12-22 13:03:34 +0100

    jquerymobile/phone vorerst fertig

--- jquerymobile/phone/android-phonegap/assets/www/js/main.js
+++ jquerymobile/phone/android-phonegap/assets/www/js/main.js
@@ -1,61 +1,117 @@
 /*
  * @TODO:
  * - Seitenübergänge zu morgen/gestern
- * - Segmentierte Liste für einzelne Mensen
- *
  *
  */
-
-
 $(document).ready(function(){
-	console.log("loaded");
 //	$('#main').live( 'pageinit',function(event){
+		date = new Date();
+		dateString = date.getFullYear() + "-" + (date.getMonth()+1) + "-" + (date.getDate());
+		dateString = "2011-12-14"; // Mock
 
-		var date = new Date();
-		var dateString = date.getFullYear() + "-" + (date.getMonth()+1) + "-" + (date.getDate());
-		storage.getMenuByDate(dateString, function(json){
+		storage.setDateFilter(dateString)
+		storage.getSortedSegmented(function(json){
 			$("#dishes").html(ich.mainList({"json" : json}));
 			$("#dishes").listview('refresh');
 		});
 //	});
 
+	$("#mensa-select").change(function(){
+		var val = $(this).val();
+		if(val == "all") storage.unsetMensaFilter();
+		else storage.setMensaFilter(val);
+		storage.getSortedSegmented(fetch);
+	});
+	$("#dish-select").change(function(){
+		var val = $(this).val();
+		if(val === "all") storage.unsetNameFilter();
+		else storage.setNameFilter(val);
+		storage.getSortedSegmented(fetch);
+	});
+
+$("#mensen form").submit(function(e){
+	e.preventDefault();
+	var inputs = $(this).serializeArray();
+	var array = [];
+	for(var i=0; i<inputs.length; i++){
+		array.push(inputs[i].value);
+	}
+
+	// Save URLs
+	conf.setURLs(array);
+
+	// Reload Data
+	storage.cleanData();
+	storage.getWeekMenu();
+
+	// Refresh Menulist
+	storage.getSortedSegmented(function(json){
+		$("#dishes").html(ich.mainList({"json" : json}));
+		$("#dishes").listview('refresh');
+	});
+
+	// Reset Mensaselect
+	setMensaSelect();
+
+	// Reset Dishselect
+	setDishSelect();
+
+	// close Dialog
+	$('.ui-dialog').dialog('close');
+});
+
+
+$('#mensen').live('pageinit', function(event){
+	var json = getMensaInfo();
+	$("#mensenlist").html(ich.mensaCheckbox({"mensen" : json})).parent().trigger("create").find("input").checkboxradio();
+});
 
 	function fetch(json){
-		 var dayNames = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Sonnabend"];
 		if(typeof json === "undefined") json = []; // @TODO: gehört hier nicht hin - storage sollte nicht "undefined zurückgeben"
-		
-		now = new Date();
-		if(date.getDate() === now.getDate()){
-			dayString = "Heute";
-		} else if(date.getDate() === now.getDate()-1){
-			dayString = "Gestern";
-		} else if(date.getDate() === now.getDate()+1){
-			dayString = "Morgen";
-		} else {
-			dayString = dayNames[date.getDay()];
-		}
-
+		var dayString = dateToString(dateString);
 		$("#header .date").text(dayString);
 		
 		$("#dishes").html(ich.mainList({"json" : json}));
 		$("#dishes").listview('refresh');
 	}
 
-		$("#next-day").click(function(e){
-			e.preventDefault();
-			date = new Date(date.valueOf() + 60 * 60 * 24 * 1000);
-			dateString = date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate();
+	$("#next-day").click(function(e){
+		e.preventDefault();
+		date = new Date(date.valueOf() + 60 * 60 * 24 * 1000);
+		dateString = date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate();
 
-			storage.getMenuByDate(dateString, fetch);
-			return;
-		});
+		storage.setDateFilter(dateString);
+		storage.getSortedSegmented(fetch);
+		return;
+	});
 
-		$("#prev-day").click(function(){
-			date = new Date(date.valueOf() - 60 * 60 * 24 * 1000);
-			dateString = date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate();
+	$("#prev-day").click(function(e){
+		e.preventDefault();
+		date = new Date(date.valueOf() - 60 * 60 * 24 * 1000);
+		dateString = date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate();
 
-			storage.getMenuByDate(dateString, fetch);
-			return;
-		});
+		storage.setDateFilter(dateString);
+		storage.getSortedSegmented(fetch);
+		return;
+	});
 
+	setMensaSelect();
+	setDishSelect();
 });
+
+
+function setMensaSelect(){
+	storage.getMensen(function(json){
+		var obj = [{name:"Alle", value:"all"}];
+		for(var i=0; i<json.length; i++) obj.push({name:json[i], value:json[i] })
+		$("#mensa-select").html(ich.selectOptions({"options" : obj})).trigger("create");
+	});
+}
+
+function setDishSelect(){
+	storage.getTypes(function(json){
+		var obj = [{name:"Alle", value:"all"}];
+		for(var i=0; i<json.length; i++) obj.push({name:json[i], value:json[i] })
+		$("#dish-select").html(ich.selectOptions({"options" : obj})).trigger("create");
+	});
+}
