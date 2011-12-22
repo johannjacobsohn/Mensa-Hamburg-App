b3be7d21b8bcc5161a922b1921fdcecce75ff03a
Johann Jacobsohn <johann.jacobsohn@directbox.com>
2011-12-22 12:58:25 +0100

    Methode conf.getMensaInfo hinzu

--- shared/js/conf.js
+++ shared/js/conf.js
@@ -26,6 +26,18 @@
 		},
 		isConfigured : function(urls){
 			return typeof localStorage.getItem("urls") === "string";
+		},
+		getMensaInfo : function(){
+			var json = [];
+			var a = this.getURLs();
+			var b = this.getSavedURLs();
+			for(i=0; i < a.length; i++){
+				json.push({
+					"name"   : a[i],
+					"active" : b.indexOf(a[i]) != -1
+				});
+			}
+			return json;
 		}
 	};
 })();
