dd2eb71e31db775a1d8a242b8e731d138419a858
Johann Jacobsohn <johannjacobsohn@Johanns-Mac-mini.local>
2012-02-23 19:58:04 +0100

    Erster Entwurf fürs iPad

---
+++ Mensa Hamburg App Tablet/www/js/nameList.js
@@ -0,0 +1,47 @@
+﻿enyo.kind({
+	name: "nameList",
+	kind: enyo.VFlexBox,
+	components: [
+		{kind: "SpinnerLarge", showing: true},
+		{kind: "Scroller", flex: 1, components: [
+			{kind: "Repeater", onSetupRow: "listSetupRow"}
+		]}
+	],
+	data : [],
+	create: function() {
+		this.inherited(arguments);
+		var that = this;
+		storage.getTypes(function(json){
+			that.$.spinnerLarge.hide();
+			that.data = json;
+			that.data.unshift("Alle");
+			that.$.repeater.render();
+		});
+	},
+	listSetupRow: function(inSender, inIndex) {
+		var row = this.data[inIndex];
+		if (row) {
+			return {kind: "Item", layoutKind: "HFlexLayout", className: (row === "Alle") ? "enyo-held" : "dummy", onclick: "itemClick", components: [
+				{content: row, flex: 1}
+			]};
+		}
+	},
+	itemClick : function(element){
+		var menuList = this.owner.$.menuList;
+		
+		for(var i=0; i<element.parent.children.length; i++){
+			element.parent.children[i].removeClass("enyo-held");
+		}
+		element.addClass("enyo-held");
+
+		if(element.children[0].content === "Alle"){
+			storage.unsetNameFilter()
+		} else {
+			storage.setNameFilter(element.children[0].content)
+		}
+		storage.getSortedSegmented(function(json){
+			menuList.data = json;
+			menuList.render();
+		});
+	}
+});
---
+++ Mensa Hamburg App Tablet/www/js/xhr.js
@@ -0,0 +1,46 @@
+/*
+ * Stellt ein primitives XMLHttpRequest-Interface zur Verfügung
+ *
+ * */
+
+(function(){
+	xhr = {
+		get : function(url, success, error, additional_args){
+			var xhr = new XMLHttpRequest();
+			xhr.open('GET', url, true);
+			xhr.onreadystatechange = function () {
+				if (xhr.readyState === 4) {
+					if(xhr.status === 200){
+						success(xhr.responseText, additional_args);
+					} else if (xhr.status === 0) {
+						success(xhr.responseText, additional_args);
+					} else if(typeof error === "function"){
+						error(xhr.responseText, additional_args);
+					}
+				}
+			};
+			xhr.send(null);
+		},
+		getJSON : function(url, success, error){
+			this.get(url, function(resp){
+				success(JSON.parse(resp));
+			}, error);
+		},
+		putJSON : function(url, json, success, error){
+			var xhr = new XMLHttpRequest();
+			xhr.open('PUT', url, true);
+			xhr.onreadystatechange = function () {
+				if (xhr.readyState === 4) {
+					if(xhr.status === 200){
+						success(xhr.responseText);
+					}
+					else if(typeof error === "function"){
+						error(xhr.responseText);
+					}
+				}
+			};
+			xhr.setRequestHeader("Content-Type", "application/json");
+			xhr.send(JSON.stringify(json));
+		}
+	};
+})();
