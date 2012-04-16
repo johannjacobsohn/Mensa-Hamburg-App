ada744be2b22d64f5c586735ba83dd4041c1b654
Johann Jacobsohn <johann.jacobsohn@directbox.com>
2012-04-16 09:34:58 +0200

    dateToString überarbeitet

--- shared/js/utils.js
+++ shared/js/utils.js
@@ -36,27 +36,58 @@ function deepCopy(o) {
 }
 
 /**
- * Create a nicely formated string representation of any given Date
+ * Create a nicely formated string representation of any given Date <br/>
  *
+ * Just a legacy wrapper for formatDate
+ * 
+ * @depreciated
+ * 
  * @param  {string} isoDate  Date in iso format (yyyy-mm-dd)
+ * @param  {string} optional language identifier, eg. "de" or "en"
  * @return {string} formated String
  */
-function dateToString(isoDate){
-	var isoDateArr = isoDate.split("-");
-	var date = new Date(isoDateArr[0], isoDateArr[1]-1, isoDateArr[2]);
+function dateToString(isoDate, lang){
+	var isoDateArr = isoDate.split("-"),
+	    date = new Date(isoDateArr[0], isoDateArr[1]-1, isoDateArr[2]);
+	
+	return formatDate(date, lang);
+}
 
-	var dayNames = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Sonnabend"];
-	var now = new Date();
-	if(date.getDate() === now.getDate()){
-		dayString = "Heute";
-	} else if(date.getDate() === now.getDate()-1){
-		dayString = "Gestern";
-	} else if(date.getDate() === now.getDate()+1){
-		dayString = "Morgen";
-	} else {
-		dayString = dayNames[date.getDay()] + ", " + date.getDate() + "." + (date.getMonth() + 1) + "." ;
-	}
-	return dayString;
+/**
+ * Create a nicely formated string representation of any given Date
+ *
+ * @param  {object} date object
+ * @param  {string} optional language identifier, eg. "de" or "en"
+ * @return {string} formated String
+ */
+function formatDate(date, lang){
+	var lang = lang || "de",
+		millisecondsInDay = 60 * 60 * 24 * 1000,
+		thatDate = Math.round(date.getTime() / millisecondsInDay),
+		thisDate = Math.round(+new Date() / millisecondsInDay),
+		dateDiff = (thatDate - thisDate),
+		dayStrings = {
+			de : {
+				"-1" : "Gestern",
+				"0"  : "Heute",
+				"1"  : "Morgen"
+			},
+			en : {
+				"-1" : "Yesterday",
+				"0"  : "Today",
+				"1"  : "Tomorrow"
+			}
+		},
+		dayNames  = {
+			"de" : ["Sonntag", "Montag", "Dienstag", "Mittwoch" , "Donnerstag", "Freitag", "Sonnabend"],
+			"en" : ["Sunday" , "Monday", "Tuesday" , "Wednesday", "Thursday"  , "Friday" , "Saturday"]
+		},
+		dateFormats = {
+			"de" : dayNames[lang][date.getDay()] + ", " + date.getDate() + "." + (date.getMonth() + 1) + ".",
+			"en" : dayNames[lang][date.getDay()] + ", " + date.getDate() + "." + (date.getMonth() + 1) + "."
+		}
+
+	return dateDiff >= -1 && dateDiff <= 1 ? dayStrings[lang][dateDiff] : dateFormats[lang];
 }
 
 /*
