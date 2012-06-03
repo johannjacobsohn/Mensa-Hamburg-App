b5e7fb85b94ba518c50ab9393245fd9da32ce14b
Johann Jacobsohn <johann.jacobsohn@directbox.com>
2012-06-03 23:03:27 +0200

    data sollte seine eigenen Daten cachen

--- shared/js/data.js
+++ shared/js/data.js
@@ -1,19 +1,24 @@
 var data = (function(){
 	var version = "3", // we might want to trigger a reload of cached data (if the)
 
+	cache = {},
+
 	get = function(key){
-		return localStorage.getItem(key);
+		return typeof cache[key] !== "undefined" ? cache[key] : localStorage.getItem(key);
 	},
 
 	set = function(key, value){
+		cache[key] = value;
 		return localStorage.setItem(key, value);
 	},
 
 	remove = function(key){
+		delete cache[key];
 		return localStorage.removeItem(key);
 	},
 
 	clear = function(){
+		cache = {};
 		return localStorage.clear();
 	}
 
