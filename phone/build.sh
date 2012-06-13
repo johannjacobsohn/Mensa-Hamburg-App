513bebaa99f8428d65fc2df5431b3d988a168b5b
Johann Jacobsohn <johann.jacobsohn@directbox.com>
2012-06-13 10:16:26 +0200

    Phone: Build-Skripte angepasst, um auf vorhandene bundle-ids Rücksicht zu nehmen

--- phone/build.sh
+++ phone/build.sh
@@ -6,15 +6,20 @@ cd ..
 
 mkdir temp
 
-cp -Lr release/* config.xml temp
+cp -Lr release/* config*.xml temp
 
 cd temp
 # todo: nutzlose Dateien entfernen und damit das zip klein halten
+# normalen Release erstellen
 rm -r lib/layout/panels/
 rm -r lib/layout/tree/
 rm -r lib/layout/list/examples/
 rm -r lib/layout/fittable/examples/
 zip -r ../release-phone.zip .
+
+# Android-Release erstellen, weil die aus historischen Gründen Android die Appid jjacobsohn.mensaApp.android hat...
+mv config.android.xml config.xml
+zip -r ../release-phone.android.zip .
 
 cd ..
 
