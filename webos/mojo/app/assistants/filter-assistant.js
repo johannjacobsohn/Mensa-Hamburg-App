93dd5f356b8fa4d8ce6033c334d225f224d3edd7
Johann Jacobsohn <johann.jacobsohn@directbox.com>
2012-09-17 08:54:37 +0200

    Mojo: Fixed filters

--- webos/mojo/app/assistants/filter-assistant.js
+++ webos/mojo/app/assistants/filter-assistant.js
@@ -13,7 +13,7 @@ FilterAssistant.prototype.activate = function(event) {
 FilterAssistant.prototype.deactivate = function(event) {
 	/* remove any event handlers you added in activate and do any other cleanup that should happen before
 	   this scene is popped or another scene is pushed on top */
-};
+	};
 
 FilterAssistant.prototype.cleanup = function(event) {
 	/* this function should do any cleanup needed before the scene is destroyed as 
@@ -21,7 +21,6 @@ FilterAssistant.prototype.cleanup = function(event) {
 };
 
 FilterAssistant.prototype.setup = function() {
-	this.model = {};
 	this.modelP = {};
 
 	// render app menu
@@ -35,142 +34,82 @@ FilterAssistant.prototype.setup = function() {
 		]
 	});
 
-	this.filterList("name", "double");
 	this.filterList("mensa", "double");
+	this.filterList("name", "double");
 	this.filterList("properties", "trippel");
 	this.filterList("additives", "trippel");
 	
 	this.controller.setupWidget('wo', { 
-		trueValue: "-1",
-		falseValue: "0",
 		modelProperty : "wo"
 	});
 	this.controller.setupWidget('with', { 
-		trueValue: "1",
-		falseValue: "0",
 		modelProperty : "with"
 	});
-
 	this.controller.setupWidget('filterToggleButton', { 
-		trueValue: "-1",
-		falseValue: "1"
+		modelProperty : "woInvers",
+		trueLabel: "Mit",
+		falseLabel: "Ohne" 
 	});
-//	this.controller.setupWidget('filterToggleButton', { trueLabel: "Mit", falseLabel: "Egal" });
-//	this.controller.setupWidget('filterRadioButton', { choices: [
-//		{label: "Ohne", value: -1},
-//		{label: "Egal", value: 0},
-//		{label: "Mit", value: 1}
-//	]});
-	
 };
 
 FilterAssistant.prototype.filterList = function(name, type){
-	var that = this;
-	that.model[name] = [];
-	that.modelP[name] = [];
-	that.controller.setupWidget("filter-" + name,
-		{ itemTemplate : "filter/filter-item-" + type }, 
-		that.modelP[name] = { items : that.model[name] } // Modell
+	this.controller.setupWidget("filter-" + name,
+		{ itemTemplate : "filter/filter-item-" + type },
+		this.modelP[name] = { items: [] } // Modell
 	);
 
-	Mojo.Event.listen(document.getElementById("filter-" + name), Mojo.Event.propertyChange, this.listPropertyChangeHandler.bind(this));
-	// In order to hear the checkbox event changes, this must be listened to
-//	Mojo.Event.listen(document.getElementById("filter-" + name), Mojo.Event.listChange, this.listPropertyChangeHandler.bind(this));
+	// listen for Event
+	Mojo.Event.listen( document.getElementById("filter-" + name),
+	                   Mojo.Event.propertyChange,
+	                   this.listPropertyChangeHandler.bind(this) );
 
-//	console.log("get " + name)
-	storage.getInfo(name, function(items){
-//		console.log("got " + name)
+	// load data
+	storage.getInfo( name, this.updateFilterList.bind(this, name) );
+}
 
-		if(name === "mensa"){
-			items = items.filter(function(item){ return item.active; })
-		}
+FilterAssistant.prototype.updateFilterList = function( name, items ){
+	// only show active mensen
+	if(name === "mensa"){
+		items = items.filter(function(item){ return item.active; })
+	}
 
-		items.map(function(item, i){
-			console.log( JSON.stringify( arguments ) );
-			item.value = item.filtered;
-			item.with = 0;
-			item.wo = 0;
-			item.filterType = name;
-			item.i = i;
-		});
-		
-		that.model[name] = items;
-		that.modelP[name].items = items;
-
-		that.controller.setWidgetModel("filter-" + name, that.modelP[name] );
+	items.forEach(function(item, i){
+		item.with = item.filter && item.filter.type === "include";
+		item.wo    = item.filter && item.filter.type === "exclude";
+		item.woInvers  = !item.wo
+		item.filterType = name;
+		item.i = i;
 	});
+
+	this.modelP[name].items = items;
+	this.controller.modelChanged( this.modelP[name] );
 }
 
 FilterAssistant.prototype.listPropertyChangeHandler = function(event){
-	var value   = event.model.value;
 	var name    = event.model.name;
 	var type    = event.model.filterType;
 	var filters = [];
 
-//	if(value === true){
-//		console.log("true")
-//		this.modelP[type].items[event.model.i].with = true;
-//	}
-
-
-// event.model.name = "Johann";
-//console.log( event.model.i )
-//console.log( "value " + this.modelP[type].items[event.model.i].wo )
-//console.log( this.modelP[type].length )
-//console.log( this.modelP[type].items[event.model.i] )
-
-//	this.modelP[type].items[event.model.i].with = 1;
-//	this.modelP[type].items[event.model.i].wo = -1;
-	//this.modelP[type].items[event.model.i].name = "Johann";
-
 	// toggle between checkboxes - there can be only one active
-	if( event.property === "with" && event.value == 1 ){
-		this.modelP[type].items[event.model.i].wo = 0;
-		filters.push( { value: event.model.name, type: "required" } )
-	} else if( event.property === "wo" && event.value == -1 ){
-		this.modelP[type].items[event.model.i].with = 0
-	} else {
-		 // none is active
-		 
+	if( event.property === "with" && event.value === true ){
+		this.modelP[type].items[event.model.i].wo = false;
+	} else if( event.property === "wo" && event.value === false ){
+		this.modelP[type].items[event.model.i].with = false
 	}
+
+	this.modelP[type].items.forEach(function(item){
+		if( item.with ){
+			filters.push( { value: item.name, type: "include" } )
+		} else if( item.wo ){
+			filters.push( { value: item.name, type: "exclude" } )
+		}
+	});
 	
 	// reload model
 	this.controller.modelChanged( this.modelP[type] );
 	
-	
-//	this.controller.revealElement( event.item );
-//	this.mojo.invalidateItems(0);
-
-//	this.mojo.revealItem( event.item )
-//	for( k in event ){
-//		console.log(k)
-//		console.log(event[k])
-//		console.log("---")
-//	}
-//	console.log(JSON.stringify(event.model));
-//	storage.setFilter("additives", )
-	
-//	type = event.model.value === 1 ? "include" : "exclude";
-//	console.log( name + ": " + value );
-//	console.log( type );
-//	console.log( JSON.stringify( this.model[type] ) );
-//	for( item in event.model ){
-//		console.log( item );
-//		console.log( event.model[item] );
-//		console.log("---")
-//		if( item.val )
-//		filters.push({value: item.value, type: "exclude"})
-//	}
-
-
-//	this.model[type].map(function(item){
-//		console.log( item.value );
-//		if( item.val )
-//		filters.push({value: item.value, type: "exclude"})
-//	});
-
-//	console.log( JSON.stringify( filters ) )
+//	console.log( "set " + type )
+//	console.log( JSON.stringify(filters) )
 
 	storage.setFilter(type, filters);
 }
-
