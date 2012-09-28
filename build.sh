c72405176814cd1fea888293477e09d45156e93e
Johann Jacobsohn <johann.jacobsohn@directbox.com>
2012-09-28 21:13:49 +0200

    Improved build script

--- build.sh
+++ build.sh
@@ -31,32 +31,42 @@ fi;
 #########
 # Phone #
 #########
-if [ $build == "phone" ] || [ $build = "all" ]; then
+if [ $build == "phone" ] || [ $build == "phone-ios" ] || [ $build == "phone-android" ] || [ $build = "all" ]; then
 	cd phone
 	./build.sh
 	cd ..
-	# phone
-	curl -u johann.jacobsohn@directbox.com -X PUT -F file=@phone/release-phone.zip https://build.phonegap.com/api/v1/apps/${ids[phone]}
+	# iphone
+	if [ $build == "phone-ios" ] || [ $build == "phone" ] || [ $build = "all" ]; then
+		curl -u johann.jacobsohn@directbox.com -X PUT -F file=@phone/release-phone.zip https://build.phonegap.com/api/v1/apps/${ids[phone]}
+	fi;
 	# android-phone
-#	curl -u johann.jacobsohn@directbox.com -X PUT -F file=@phone/release-phone.android.zip https://build.phonegap.com/api/v1/apps/${ids[phoneAndroid]}
+	if [ $build == "phone-android" ] || [ $build == "phone" ] || [ $build = "all" ]; then
+		curl -u johann.jacobsohn@directbox.com -X PUT -F file=@phone/release-phone.android.zip https://build.phonegap.com/api/v1/apps/${ids[phoneAndroid]}
+	fi;
 fi;
 
 ##########
 # Tablet #
 ##########
-if [ $build = "tablet" ] || [ $build = "all" ]; then
+if [ $build = "tablet" ] || [ $build == "tablet-ios" ] || [ $build == "tablet-android" ] || [ $build = "all" ]; then
 	cd tablet
 	./build.sh
 	cd ..
-	curl -u johann.jacobsohn@directbox.com -X PUT -F file=@tablet/release-tablet.zip https://build.phonegap.com/api/v1/apps/${ids[tablet]}
-	curl -u johann.jacobsohn@directbox.com -X PUT -F file=@tablet/release-tablet.android.zip https://build.phonegap.com/api/v1/apps/${ids[tabletAndroid]}
+	# ipad
+	if [ $build == "tablet-ios" ] || [ $build == "tablet" ] || [ $build = "all" ]; then
+		curl -u johann.jacobsohn@directbox.com -X PUT -F file=@tablet/release-tablet.zip https://build.phonegap.com/api/v1/apps/${ids[tablet]}
+	fi;
+	# android tablet
+	if [ $build == "tablet-android" ] || [ $build == "tablet" ] || [ $build = "all" ]; then
+		curl -u johann.jacobsohn@directbox.com -X PUT -F file=@tablet/release-tablet.android.zip https://build.phonegap.com/api/v1/apps/${ids[tabletAndroid]}
+	fi;
 fi;
 
 #########
 # AppUp #
 #########
 if [ $build = "appup" ] || [ $build = "all" ]; then
-echo "todo"
+	echo "todo"
 fi;
 
 ###########
@@ -66,5 +76,15 @@ fi;
 
 
 
+
+#################
+# Google Chrome #
+#################
+if [ $build = "chrome-release" ] || [ $build = "chrome" ] || [ $build = "all" ]; then
+	google-chrome --pack-extension=chrome --pack-extension-key=/home/jjacobsohn/Desktop/mensa/release/chrome.pem
+fi;
+if [ $build = "chrome-test" ] || [ $build = "chrome" ] || [ $build = "all" ]; then
+	google-chrome --pack-extension=chrome --pack-extension-key=/home/jjacobsohn/Desktop/mensa/release/chrome.pem
+fi;
 
 exit;
