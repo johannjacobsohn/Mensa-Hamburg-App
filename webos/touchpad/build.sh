e5c94e6e2122699427789537cf45f7f77ab00f14
Johann Jacobsohn <johann.jacobsohn@directbox.com>
2012-11-09 22:45:38 +0100

    New Touchpad version; cleaned up webos folder

---
+++ webos/touchpad/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# build enyo
+rm ../../enyo2/tablet/deploy/* -r 
+cd ../../enyo2/tablet/tools
+./deploy.sh
+cd -
+mv ../../enyo2/tablet/deploy/* build
+cp Makefile appinfo.json framework_config.json build
+cd build
+make
+mv jjacobsohn.mensaapp.enyo_*_all.ipk ..
+cd -
+
+rm -r build
+
+exit 1
