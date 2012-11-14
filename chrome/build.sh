ba3b3f9bcdd5505e84475484f3f90b5f93d37fe8
Johann Jacobsohn <johann.jacobsohn@directbox.com>
2012-11-14 22:12:08 +0100

    build chrome web app

---
+++ chrome/build.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+set -e # abort on error
+set -u # abort on undefined
+set -x # verbose
+
+# clear old builds
+set +e
+rm -r build
+rm -r ../enyo2/tablet/deploy/*
+set -e
+
+cd ../enyo2/tablet/tools
+./deploy.sh
+cd -
+mv ../enyo2/tablet/deploy/* build
+cp -L manifest.json icon-16.png icon-48.png icon-128.png build
+
+chromium-browser --pack-extension=build --pack-extension-key=../keys/chrome/chrome.pem
