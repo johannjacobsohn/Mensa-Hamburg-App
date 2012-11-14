2cbbd4bdb1c2c3d5abce9a59b085fb4c316a8c06
Johann Jacobsohn <johann.jacobsohn@directbox.com>
2012-11-14 18:04:29 +0100

    fixed config

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
+http_path = "/Mensa-Hamburg-App/"
 http_images_path = "/Mensa-Hamburg-App/images"
 http_fonts_path = "/Mensa-Hamburg-App/fonts"
-css_dir = "public/stylesheets"
+css_dir = "public/Mensa-Hamburg-App/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
