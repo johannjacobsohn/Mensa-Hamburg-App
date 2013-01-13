aad565437f5140310e230ca104cb6535d0c25068
Johann Jacobsohn <johann.jacobsohn@directbox.com>
2013-01-13 01:01:41 +0100

    Add Polyfills for Array.prototype.some and addEventListener

--- shared/js/utils.js
+++ shared/js/utils.js
@@ -372,3 +372,60 @@ if (!Array.prototype.filter)
     return res;  
   };  
 }  
+
+//https://gist.github.com/2864711
+//addEventListener polyfill 1.0 / Eirik Backer / MIT Licence
+(function(win, doc){
+	if(win.addEventListener)return;		//No need to polyfill
+ 
+	function docHijack(p){var old = doc[p];doc[p] = function(v){return addListen(old(v))}}
+	function addEvent(on, fn, self){
+		return (self = this).attachEvent('on' + on, function(e){
+			var e = e || win.event;
+			e.preventDefault  = e.preventDefault  || function(){e.returnValue = false}
+			e.stopPropagation = e.stopPropagation || function(){e.cancelBubble = true}
+			fn.call(self, e);
+		});
+	}
+	function addListen(obj, i){
+		if(i = obj.length)while(i--)obj[i].addEventListener = addEvent;
+		else obj.addEventListener = addEvent;
+		return obj;
+	}
+ 
+	addListen([doc, win]);
+	if('Element' in win)win.Element.prototype.addEventListener = addEvent;			//IE8
+	else{		//IE < 8
+		doc.attachEvent('onreadystatechange', function(){addListen(doc.all)});		//Make sure we also init at domReady
+		docHijack('getElementsByTagName');
+		docHijack('getElementById');
+		docHijack('createElement');
+		addListen(doc.all);	
+	}
+})(window, document);
+
+// https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Array/some
+if (!Array.prototype.some)
+{
+  Array.prototype.some = function(fun /*, thisp */)
+  {
+    "use strict";
+ 
+    if (this == null)
+      throw new TypeError();
+ 
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if (typeof fun != "function")
+      throw new TypeError();
+ 
+    var thisp = arguments[1];
+    for (var i = 0; i < len; i++)
+    {
+      if (i in t && fun.call(thisp, t[i], i, t))
+        return true;
+    }
+ 
+    return false;
+  };
+}
