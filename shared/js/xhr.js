9ee777e9846a3bc101cf07da5c4515e55c9085c4
Johann Jacobsohn <johann.jacobsohn@directbox.com>
2013-01-13 01:06:55 +0100

    xhr: add fixes for IE8

--- shared/js/xhr.js
+++ shared/js/xhr.js
@@ -5,6 +5,25 @@
  * @class xhr
  */
 (function(){
+	// http://www.html5rocks.com/en/tutorials/cors/
+	function createCORSRequest(method, url) {
+		var xhr = new XMLHttpRequest();
+		if ("withCredentials" in xhr) {
+			// Check if the XMLHttpRequest object has a "withCredentials" property.
+			// "withCredentials" only exists on XMLHTTPRequest2 objects.
+			xhr.open(method, url, true);
+		} else if (typeof XDomainRequest !== "undefined") {
+			// Otherwise, check if XDomainRequest.
+			// XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+			xhr = new XDomainRequest();
+			xhr.open(method, url);
+		} else {
+			// Otherwise, CORS is not supported by the browser.
+			xhr = null;
+		}
+		return xhr;
+	}
+
 	xhr = {
 		/**
 		 * Make a HTTP-Get-Request
@@ -16,18 +35,9 @@
 		 * @param additional_args
 		 */
 		get : function(url, success, error, additional_args){
-			var xhr = new XMLHttpRequest();
-			xhr.open('GET', url, true);
-			xhr.onreadystatechange = function () {
-				if (xhr.readyState === 4) {
-					if(xhr.status === 200){
-						success(xhr.responseText, additional_args);
-					} else if (xhr.status === 0) {
-						success(xhr.responseText, additional_args);
-					} else if(typeof error === "function"){
-						error(xhr.responseText, additional_args);
-					}
-				}
+			var xhr = createCORSRequest("GET", url);
+			xhr.onload = function() {
+				success(xhr.responseText, additional_args);
 			};
 			xhr.send(null);
 		},
