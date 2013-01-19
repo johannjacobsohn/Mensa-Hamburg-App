0e4137fa0fe9706da6560b9bb560ff7070e7b52b
Johann Jacobsohn <johann.jacobsohn@directbox.com>
2013-01-19 12:14:01 +0100

    Add dev channel for webapp

--- webapp/build.sh
+++ webapp/build.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 #
+# usage: build.sh (phone|tablet) [release]
 #
-
 set -u
 set -x
 set -e
 
 type=$1
+release=${2-dev}
 
 path="../enyo2/$type"
 build="$path/deploy/$type";
-target="$type";
+if [ $release == "release" ]; then
+	target="$type";
+else
+	target="$type-dev";
+fi
 
 # clear old builds
 set +e
@@ -21,6 +26,7 @@ cd $path
 tools/deploy.sh
 cd -
 mv $build $target
+
 cp 64x64.png 72x72.png 144x144.png app.appcache landscape-ipad.png portrait-ipad.png index.html favicon.ico $target
 mkdir -p $target/lib/Carousels
 touch $target/lib/Carousels/newness.Carousels.js $target/lib/Carousels/package.js $target/assets/icn-share.png $target/assets/icon-email.png $target/assets/shadow.png
@@ -28,4 +34,3 @@ touch $target/lib/Carousels/newness.Carousels.js $target/lib/Carousels/package.j
 rsync -r $target/ root@$target.mensaapp.org:$target/
 
 exit;
-
