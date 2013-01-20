1324b296b6013c71fe9bb96393328d827e3bf4ec
Johann Jacobsohn <johann.jacobsohn@directbox.com>
2013-01-20 19:50:50 +0100

    Add support for Firefox (OS) to Webapp

--- webapp/build.sh
+++ webapp/build.sh
@@ -22,15 +22,28 @@ set +e
 rm -r $target
 rm -r $build
 set -e
+
+# build app
 cd $path
 tools/deploy.sh
 cd -
 mv $build $target
 
-cp 64x64.png 72x72.png 144x144.png app.appcache landscape-ipad.png portrait-ipad.png index.html favicon.ico $target
+# copy icons
+cp 64x64.png 72x72.png icon-128.png icon-60.png 144x144.png favicon.ico $target
+
+# copy manifests and other files
+cp app.appcache landscape-ipad.png portrait-ipad.png index.html $target
+cp .htaccess $target
+
+# copy version specific files
+cp manifest.webapp.$target $target/manifest.webapp
+
+# mock version specific files (so that we only need one appcache manifest)
 mkdir -p $target/lib/Carousels
 touch $target/lib/Carousels/newness.Carousels.js $target/lib/Carousels/package.js $target/assets/icn-share.png $target/assets/icon-email.png $target/assets/shadow.png
 
+# upload new version
 rsync -r $target/ root@$target.mensaapp.org:$target/
 
 exit;
