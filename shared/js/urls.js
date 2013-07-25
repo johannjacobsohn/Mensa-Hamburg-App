82cd34ea4454e5b869d7b13b696bef64b6aebed7
Johann Jacobsohn <j.jacobsohn@satzmedia.de>
2013-07-25 22:50:34 +0200

    fix urls

--- shared/js/urls.js
+++ shared/js/urls.js
@@ -10,103 +10,103 @@
 			{
 				name: "Alexanderstrasse",
 				id: "alexanderstrasse",
-				url: baselink + "alexanderstrasse/{{week}}",
+				url: baselink + "Alexanderstrasse/{{week}}",
 				address: "Alexanderstraße 1, 20099 Hamburg"
 			},
 			{
 				name: "Armgartstrasse",
 				id: "armgartstrasse",
-				url: baselink + "armgartstrasse/{{week}}/",
+				url: baselink + "Armgartstrasse/{{week}}/",
 				address: "Armgartstraße 24, 22087 Hamburg"
 			},
 			{
 				name: "Averhoffstrasse",
 				id: "averhoffstrasse",
-				url: baselink + "averhoffstrasse/{{week}}/",
+				url: baselink + "Averhoffstrasse/{{week}}/",
 				address: "Averhoffstraße 38, 22085 Hamburg"
 			},
 			{
 				name: "Bergedorf",
 				id: "bergedorf",
-				url: baselink + "bergedorf/{{week}}/",
+				url: baselink + "Bergedorf/{{week}}/",
 				address: "Lohbrügger Kirchstraße 65, 21033 Hamburg"
 			},
 			{
 				name: "Berliner Tor",
 				id: "berlinertor",
-				url: baselink + "berlinertor/{{week}}/",
+				url: baselink + "BerlinerTor/{{week}}/",
 				address: "Berliner Tor 7, 20099 Hamburg"
 			},
 			{
 				name: "Botanischer Garten",
 				id: "botanischergarten",
-				url: baselink + "botanischergarten/{{week}}/",
+				url: baselink + "BotanischerGarten/{{week}}/",
 				address: "Ohnhorstraße 18, 22609 Hamburg"
 			},
 			{
 				name: "Bucerius Law School",
 				id: "buceriuslawschool",
-				url: baselink + "buceriuslawschool/{{week}}/",
+				url: baselink + "BuceriusLawSchool/{{week}}/",
 				address: "Jungiusstraße 6, 20355 Hamburg"
 			},
 			{
 				name: "Campus",
 				id: "campus",
-				url: baselink + "campus/{{week}}/",
+				url: baselink + "Campus/{{week}}/",
 				address: "Von-Melle-Park 5, 20146 Hamburg"
 			},
 			{
 				name: "City Nord",
 				id: "citynord",
-				url: baselink + "citynord/{{week}}/",
+				url: baselink + "CityNord/{{week}}/",
 				address: "Hebebrandstraße 1, 22297 Hamburg"
 			},
 			{
 				name: "Finkenau",
 				id: "finkenau",
-				url: baselink + "finkenau/{{week}}/",
+				url: baselink + "Finkenau/{{week}}/",
 				address: "Finkenau 35, 22081 Hamburg"
 			},
 			{
 				name: "Geomatikum",
 				id: "geomatikum",
-				url: baselink + "geomatikum/{{week}}/",
+				url: baselink + "Geomatikum/{{week}}/",
 				address: "Bundesstraße 55, 20146 Hamburg"
 			},
 			{
 				name: "Harburg",
 				id: "harburg",
-				url: baselink + "harburg/{{week}}/",
+				url: baselink + "Harburg/{{week}}/",
 				address: "Denickestraße 22, 21073 Hamburg"
 			},
 			{
 				name: "Jungiusstrasse",
 				id: "jungiusstrasse",
-				url: baselink + "jungiusstrasse/{{week}}/",
+				url: baselink + "Jungiusstrasse/{{week}}/",
 				address: "Jungiusstraße 9, 20355 Hamburg"
 			},
 			{
 				name: "Philosophenturm",
 				id: "philosophenturm",
-				url: baselink + "philosophenturm/{{week}}/",
+				url: baselink + "Philosophenturm/{{week}}/",
 				address: "Von-Melle-Park 6, 20146 Hamburg"
 			},
 			{
 				name: "Stellingen",
 				id: "stellingen",
-				url: baselink + "stellingen/{{week}}/",
+				url: baselink + "Stellingen/{{week}}/",
 				address: "Vogt-Kölln-Straße 30, 22527 Hamburg"
 			},
 			{
 				name: "Studierendenhaus",
 				id: "studierendenhaus",
-				url: baselink + "studierendenhaus/{{week}}/",
+				url: baselink + "Studierendenhaus/{{week}}/",
 				address: "Von-Melle-Park 2, 20146 Hamburg"
 			},
 			{
 				name: "Cafe CFEL",
 				id: "cafecfel",
-				url: baselink + "cafecfel/{{week}}/",
+				url: baselink + "CafeCFEL/{{week}}/",
 				address: "Notkestrasse 85, 22607 Hamburg"
 			}
 		]
